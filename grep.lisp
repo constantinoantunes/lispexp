94b1d79e324e64e338594db2d6e9c3ce97de9254
Constantino <constantino.antunes@gmail.com>
2015-10-10 14:15:47 +0100

    Fix typo in usage

--- grep.lisp
+++ grep.lisp
@@ -1,7 +1,7 @@
 #!/usr/bin/env clisp
 
 (if (not (eq (length *args*) 2)) (progn
-  (format t "Usage: grep.lisp <regrex> <file>")
+  (format t "Usage: grep.lisp <regex> <file>")
   (exit)))
 
 (let ((regex (nth 0 *args*)) (fh (open (nth 1 *args*))))
