47e48b883e6ea580498c6cfba596475671cd4ed2
Constantino <constantino.antunes@gmail.com>
2015-10-10 18:50:56 +0100

    Fix macro line reference
    Previously, if the variable 'line' name changed, execution would break.
    Now the macro receives the 'line' variable name an uses it. Also, now
    the syntax looks more like a new language.

--- grep.lisp
+++ grep.lisp
@@ -4,12 +4,12 @@
   (format t "Usage: grep.lisp <regex> <file>")
   (exit 1)))
 
-(defmacro foreach-line-in-file (filename &rest body)
+(defmacro foreach (line in file filename &rest body)
   `(let ((file_handle (open ,filename)))
-    (when file_handle (loop for line = (read-line file_handle nil)
-      while line do ,@body))
+    (when file_handle (loop for ,line = (read-line file_handle nil)
+      while ,line do ,@body))
     (close file_handle)))
 
 (let ((regex (nth 0 *args*)) (filename (nth 1 *args*)))
-  (foreach-line-in-file filename
+  (foreach line in file filename
     (if (regexp:match regex line) (format t "~a~%" line))))
